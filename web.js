a3824005e1509d10bedd497fb4e6e9e6c413a044
nbalba <nbalba@gmail.com>
2013-12-20 06:11:24 -0500

    added connect to DATABASE_URL

--- web.js
+++ web.js
@@ -9,6 +9,17 @@ app.get('/', function(req, res) {
   res.send('Hello World - This is me!');
 });
 
+var pg = require('pg');
+
+pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  client.query('SELECT * FROM your_table', function(err, result) {
+    done();
+    if(err) return console.error(err);
+    console.log(result.rows);
+  });
+});
+
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
