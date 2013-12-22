007558b45e26beea0f2c164fd0da13a4c2aed63d
nbalba <nbalba@gmail.com>
2013-12-22 08:48:01 -0500

    init

--- web.js
+++ web.js
@@ -19,33 +19,6 @@ pg.connect(process.env.DATABASE_URL, function(err, client, done) {
   });
 });
 
-var compare = function(choice1, choice2) {
-    if (choice1 === choice2) {
-        return "The result is a tie!";
-    } 
-    if (choice1 === "rock") {
-        if (choice2 === scissors) {
-            return "rock wins";
-        } else {
-            return "paper wins";
-        }
-    }
-    if (choice1 === "paper") {
-        if (choice2 === scissors) {
-            return "scissors wins";
-        } else {
-            return "paper wins";
-        }
-    }
-    if (choice1 === "scissors") {
-        if (choice2 === rock) {
-            return "rock wins";
-        } else {
-            return "scissors wins";
-        }
-    }
-};
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
